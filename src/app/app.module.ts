5b65a4f5d7cefdfb29d2c77e3858d2efd66278d9
ngactive99 <ngactive99@gmail.com>
2022-05-25 09:58:43 +0530

    displaying all components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSidenavModule } from '@angular/material/sidenav';
-
+import { LayoutModule } from './layout/layout.module';
 
 @NgModule({
   declarations: [
@@ -13,6 +13,7 @@ import { MatSidenavModule } from '@angular/material/sidenav';
   ],
   imports: [
     BrowserModule,
+    LayoutModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatSidenavModule
